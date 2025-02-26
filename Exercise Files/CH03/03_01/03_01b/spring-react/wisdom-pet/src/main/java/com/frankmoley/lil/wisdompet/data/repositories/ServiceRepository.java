06ae344669510e31b9af95c733e334f4ba262c63
PantelisGavalas <pantelis.g7@gmail.com>
2025-02-26 13:12:50 +0100

    Starting the UI.

--- Exercise Files/CH03/03_01/03_01b/spring-react/wisdom-pet/src/main/java/com/frankmoley/lil/wisdompet/data/repositories/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.frankmoley.lil.wisdompet.data.repositories;
-
-import com.frankmoley.lil.wisdompet.data.entities.ServiceEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ServiceRepository extends CrudRepository<ServiceEntity, Long> {
-
-}
---
+++ Exercise Files/CH03/03_01/03_01b/spring-react/wisdom-pet/src/main/java/com/pantelisgavalas/lil/wisdom_pet/data/repositories/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.pantelisgavalas.lil.wisdom_pet.data.repositories;
+
+import com.pantelisgavalas.lil.wisdom_pet.data.entities.VendorEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VendorRepository extends CrudRepository<VendorEntity, Long> {
+}
