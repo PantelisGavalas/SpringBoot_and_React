96d8e68b6a5063ce486d67a4fe5237f27f527795
PantelisGavalas <pantelis.g7@gmail.com>
2025-02-26 13:39:47 +0100

    Building our Customers component.

---
+++ Exercise Files/CH03/03_02/03_02b/spring-react/wisdom-pet/src/main/java/com/pantelisgavalas/lil/wisdom_pet/web/models/Service.java
@@ -0,0 +1,21 @@
+package com.pantelisgavalas.lil.wisdom_pet.web.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Service {
+
+    public Long serviceId;
+    public String name;
+    public BigDecimal price;
+}
--- Exercise Files/CH03/03_02/03_02b/spring-react/wisdom-pet/src/ui/src/pages/Home.js
+++ Exercise Files/CH03/03_02/03_02b/spring-react/wisdom-pet/src/ui/src/pages/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (
+    return (
     <>
-      <h3>Admin</h3>
-      <ul style={{listStyleType: 'none'}}>
-        <li><Link to={'/customers'}>Customers</Link></li>
-      </ul>
+        <h3>Admin</h3>
+        <ul style={{listStyleType: "none"}}>
+            <li><Link to={"/customers"}>Customers</Link></li>
+        </ul>
     </>
-  )
-};
+    )
+}
+
 export default Home;
